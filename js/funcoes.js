f4a1c191d20d7b8b499b7cfebe5c3e9e2577bc76
highborne <evertonluizcoster@gmail.com>
2020-03-21 16:49:08 -0300

    Calculo feito!

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,10 +1,67 @@
 function calcular() {
+    event.preventDefault();
+
+    const resultado = document.querySelector('.resultado');
+    var quantidade_carne, quantidae_vegetais, quatidade_cerveja, quatidade_refri, html;
     var pessoas = document.getElementById("quantidade_pessoas");
     var horas = document.getElementById("tempo_churrasco");
     var vegetarianos = document.getElementById("quantidade_vegetarianos");
 
-    if (pessoas.value != "") {
-        alert('Obrigado sr(a) ' + pessoas.value + ' os seus dados foram encaminhados com sucesso');
+
+    if (pessoas.value == "" ) {
+        alert("Preencha todos os dados!!!");
+    }else{
+        quantidade_carne = ((parseInt(pessoas.value) * 50) * horas.value) / 1000 ;
+        quantidae_vegetais = ((pessoas.value * 50) + (vegetarianos.value * 100) * horas.value) / 1000;
+        quatidade_cerveja = (parseInt(pessoas.value) + parseInt(vegetarianos.value)) * horas.value;
+        quatidade_refri = ((parseInt(pessoas.value) + parseInt(vegetarianos.value)) * 100 ) * horas.value / 1000;
+
+        html +=`
+        <div class="row">
+          <div class="carne col-md-3 col-sm-12">
+            <p>Carne</p>
+            <p class="tipo">${quantidade_carne} KG</p>
+          </div>
+          <div class="carne col-md-3 col-sm-12">
+            <p>Vegetais</p>
+            <p class="tipo">${quantidae_vegetais} KG</p>
+          </div>
+          <div class="carne col-md-3 col-sm-12">
+            <p>Cerveja</p>
+            <p class="tipo">${quatidade_cerveja} Latas</p>
+          </div>
+          <div class="carne col-md-3 col-sm-12">
+            <p>Refrigerante</p>
+            <p class="tipo">${quatidade_refri} Litros</p>
+          </div>
+        </div>
+
+
+        `
+
+
     }
 
+  resultado.innerHTML = html;
+
 }
+
+
+
+function cria_cards(jsonObj){
+        var quartos = jsonObj;
+        var html = "";
+        console.log(quartos);
+
+        for(i = 0; i < quartos.length; i++){
+          html +=`
+          <div class="item">
+                  <img class="img" src="${quartos[i].photo}">
+                  <p class="tipo">${quartos[i].property_type}</p>
+                  <p class="nome">${quartos[i].name}</p>
+                  <p class="preco">${quartos[i].price}</p>
+          </div>`
+                  console.log(html)
+      }
+      section.innerHTML = html;
+    }
