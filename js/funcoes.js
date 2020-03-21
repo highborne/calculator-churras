c733d35eb524e18cb29254312a8e50c1d4c05810
highborne <evertonluizcoster@gmail.com>
2020-03-21 19:39:33 -0300

    Correção calculos

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,46 +1,17 @@
 function calcular() {
     event.preventDefault();
-
     const resultado = document.querySelector('.resultado');
-    var quantidade_carne, quantidae_vegetais, quatidade_cerveja, quatidade_refri, html;
-    var pessoas = document.getElementById("quantidade_pessoas");
-    var horas = document.getElementById("tempo_churrasco");
-    var vegetarianos = document.getElementById("quantidade_vegetarianos");
 
+    let pessoas = document.getElementById("quantidade_pessoas");
+    let horas = document.getElementById("tempo_churrasco");
+    let vegetarianos = document.getElementById("quantidade_vegetarianos");
+    let quantidade_carne = ((parseInt(pessoas.value) * 50) * horas.value) / 1000;
+    let quantidae_vegetais = ((pessoas.value * 50) + (vegetarianos.value * 100) * horas.value) / 1000;
+    let quantidade_cerveja = (parseInt(pessoas.value) + parseInt(vegetarianos.value)) * horas.value;
+    let quantidade_refri = (((parseInt(pessoas.value) + parseInt(vegetarianos.value)) * 100 ) * horas.value)  /1000;
 
-    if (pessoas.value == "" ) {
-        alert("Preencha todos os dados!!!");
-    }else{
-        quantidade_carne = ((parseInt(pessoas.value) * 50) * horas.value) / 1000 ;
-        quantidae_vegetais = ((pessoas.value * 50) + (vegetarianos.value * 100) * horas.value) / 1000;
-        quatidade_cerveja = (parseInt(pessoas.value) + parseInt(vegetarianos.value)) * horas.value;
-        quatidade_refri = ((parseInt(pessoas.value) + parseInt(vegetarianos.value)) * 100 ) * horas.value / 1000;
-
-        html +=`
-        <div class="row">
-          <div class="carne col-md-3 col-sm-12">
-            <p>Carne</p>
-            <i class="fas fa-bacon"></i>
-            <p class="tipo">${quantidade_carne} KG</p>
-          </div>
-          <div class="carne col-md-3 col-sm-12">
-            <p>Vegetais</p>
-            <i class="fas fa-seedling"></i>
-            <p class="tipo">${quantidae_vegetais} KG</p>
-          </div>
-          <div class="carne col-md-3 col-sm-12">
-            <p>Cerveja</p>
-            <i class="fas fa-beer"></i>
-            <p class="tipo">${quatidade_cerveja} Latas</p>
-          </div>
-          <div class="carne col-md-3 col-sm-12">
-            <p>Refrigerante</p>
-            <i class="fas fa-wine-bottle"></i>
-            <p class="tipo">${quatidade_refri} Litros</p>
-          </div>
-        </div>
-        `
-    }
-
-  resultado.innerHTML = html;
+    resultado.getElementsByClassName('valor')[0].innerHTML = quantidade_carne;
+    resultado.getElementsByClassName('valor')[1].innerHTML = quantidae_vegetais;
+    resultado.getElementsByClassName('valor')[2].innerHTML = quantidade_cerveja;
+    resultado.getElementsByClassName('valor')[3].innerHTML = quantidade_refri;
 }
