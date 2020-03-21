2dc68d1b5114dd306b2dd6e44668273250beef6c
highborne <evertonluizcoster@gmail.com>
2020-03-21 15:57:19 -0300

    js

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,1 +1,10 @@
+function calcular() {
+    var pessoas = document.getElementById("quantidade_pessoas");
+    var horas = document.getElementById("tempo_churrasco");
+    var vegetarianos = document.getElementById("quantidade_vegetarianos");
 
+    if (pessoas.value != "") {
+        alert('Obrigado sr(a) ' + pessoas.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
